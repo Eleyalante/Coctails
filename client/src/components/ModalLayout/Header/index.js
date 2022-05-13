03efe6775731bc72cabc545ad31fa6fc0b644fdc
NgQHung <88713708+NgQHung@users.noreply.github.com>
2022-05-13 15:16:43 +0200

    add page Add and Modal

---
+++ client/src/components/ModalLayout/Header/index.js
@@ -0,0 +1,28 @@
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+const cx = classNames.bind(styles);
+
+function Header() {
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("back-btn")}>
+          <a href='/'>
+            <FontAwesomeIcon icon={faChevronLeft} />
+            Home Page
+          </a>
+        </div>
+        <div className={cx("logo")}>
+          <button>
+            <a href='/'>Logo</a>
+          </button>
+          Let your imagination fly
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -1,6 +1,12 @@
 import React from "react";
+import ModalLayout from "../components/ModalLayout";
 
 function Add() {
-  return <div>add</div>;
+  return (
+    <div>
+      <ModalLayout />
+    </div>
+  );
 }
+
 export default Add;
