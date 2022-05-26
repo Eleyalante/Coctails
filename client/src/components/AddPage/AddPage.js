7d9c190f20a6a6250a4afccf76a5f78e56a04cde
NgQHung <88713708+NgQHung@users.noreply.github.com>
2022-05-26 10:14:40 +0200

    add delete modal

--- client/src/components/AddPage/AddPage.js
+++ client/src/components/AddPage/AddPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./AddPage.scss";
 import ScnHeader from "../../common/ScnHeader/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
 function AddPage() {
   return (
     <>
@@ -40,7 +40,13 @@ function AddPage() {
               </button>
             </div>
           </div>
-          <div className='ingredients'>
+          <div className='add-btn'>
+            <button>
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+          </div>
+
+          {/* <div className='ingredients'>
             <div className='amount'>
               <p>Amount</p>
               <input placeholder='...' spellCheck={false}></input>
@@ -165,12 +171,12 @@ function AddPage() {
                 <FontAwesomeIcon icon={faTrash} />
               </button>
             </div>
-          </div>
+          </div> */}
           {/* ////////////////////////////////////////////////////////// */}
 
           <div className='save-btn'>
             <button>
-              <a href='/mycocktails'>Add recipe</a>
+              <a href='/mycocktails'>Save recipe</a>
             </button>
           </div>
         </div>
--- client/src/components/MyCocktailsPage/MyCocktailsPage.js
+++ client/src/components/MyCocktailsPage/MyCocktailsPage.js
@@ -6,6 +6,24 @@ function MyCocktailsPage() {
   return (
     <>
       <ScnHeader />
+      <section className='home-cocktail'>
+        <div className='title'>
+          <h3>My cocktail</h3>
+          <a href='/mycocktails/trash'>Trash bin</a>
+        </div>
+        <div className='table'>
+          <table class='table-box'>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>CreateAT</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </section>
     </>
   );
 }
